3b2e16b4c6cf4c5a1875340cec3afaec18744dcd
VadenPro <yariktiyarik@gmail.com>
2022-08-31 22:22:12 +0300

    implement user and token api

--- app/core/admin.py
+++ app/core/admin.py
@@ -10,15 +10,30 @@ class UserAdmin(BaseUserAdmin):
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
         (_('Personal info'), {'fields': ('name',)}),
-        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        (_('Permissions'), {'fields': (
+            'is_active',
+            'is_staff',
+            'is_superuser',
+            'groups',
+            'user_permissions'
+        )}),
         (_('Important dates'), {'fields': ['last_login']})
     )
     readonly_fields = ['last_login']
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2', 'name', 'is_active', 'is_staff', 'is_superuser')
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser'
+            )
         }),
     )
+
 
 admin.site.register(models.User, UserAdmin)
